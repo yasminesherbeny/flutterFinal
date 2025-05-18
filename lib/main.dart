bcfdd7f7d7f53e385d9d9de51bdff34986149201
lailaamrk <115193721+lailaamrk@users.noreply.github.com>
2025-05-18 11:14:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: DashboardScreen(),
+      home: NavBar(),
     );
   }
 }
